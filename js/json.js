112320f785f30350e7a3ef2c00e002630e0bef8e
Michael Bain <mbain@fullsail.edu>
2013-05-16 12:05:19 -0400

    Project 2 finished

---
+++ js/json.js
@@ -0,0 +1,406 @@
+//Michael Bain
+//VFW 1302
+//2/26/13
+//Json data
+
+
+var json = {
+	"sessionInfo1":  {
+		"optgroup":  ["Sampling:", "intro"],
+		"uname":     ["User:", "Mark Gonzales"],
+		"email":     ["Email:", "thegonz@gmail.com"],
+		"phone":     ["Phone:", "718-364-0987"],
+		"album":     ["Album:", "Bitches Brew"],
+		"song":      ["Song:", "Spanish Key"],
+		"artist":    ["Artist:", "Miles Davis"],
+		"label":     ["Label:", "Columbia"],
+		"year":      ["Year:", "1970"],
+		"date":      ["Date:", "2012-22-4"],
+		"store":     ["Store:", "Blecker Bob's"],
+		"minutes":   ["Minutes:", "5"],
+		"comments":  ["Comments:", "Used the drums."],
+		"imported":  ["Import:", "No"],
+		"radio":     ["Release:", "Reissue"]
+		
+				
+	},
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "chorus"],
+     	"uname":     ["User:", "Kendrick Cross"],
+     	"email":     ["Email:", "Kendrick@gmail.com"],
+     	"phone":     ["Phone:", "718-909-3546"],
+     	"album":     ["Album:", "Someone Else's Arms"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Brunswick"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-23-4"],
+     	"store":     ["Store:", "Other Music"],
+     	"minutes":   ["Minutes:", "3"],
+     	"comments":  ["Comments:", "Rhodes sample."],
+     	"imported":  ["Import:", "Yes"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     },
+     "sessionInfo3": {
+     	"optgroup":  ["Sampling:", "bridge"],
+     	"uname":     ["User:", "Brian Schaeffer"],
+     	"email":     ["Email:", "Schaeffer@gmail.com"],
+     	"phone":     ["Phone:", "647-364-7676"],
+     	"album":     ["Album:", "Long Stroke"],
+     	"song":      ["Song:", "Funky Combo"],
+     	"artist":    ["Artist:", "ADC Band"],
+     	"label":     ["Label:", "Cotillion"],
+     	"year":      ["Year:", "1978"],
+     	"date":      ["Date:", "2012-22-8"],
+     	"store":     ["Store:", "St.Marks Music"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Guitar sample."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Original"]
+
+     
+     },
+     "sessionInfo4": {
+     	"optgroup":  ["Sampling:", "verse"],
+     	"uname":     ["User:", "John Eddins"],
+     	"email":     ["Email:", "Eddins@gmail.com"],
+     	"phone":     ["Phone:", "212-345-6789"],
+     	"album":     ["Album:", "Straight From The Gate"],
+     	"song":      ["Song:", "Pork Soda"],
+     	"artist":    ["Artist:", "The Headhunters"],
+     	"label":     ["Label:", "Arista"],
+     	"year":      ["Year:", "1977"],
+     	"date":      ["Date:", "2012-20-8"],
+     	"store":     ["Store:", "Bleecker Street"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     		
+     		
+	  },
+	  "sessionInfo5":{
+	  	"optgroup":  ["Sampling:", "intro"],
+	  	"uname":     ["User:", "Tom Clark"],
+	  	"email":     ["Email:", "Clark@gmail.com"],
+	  	"phone":     ["Phone:", "647-364-5463"],
+	  	"album":     ["Album:", "This Is Your Life"],
+	  	"song":      ["Song:", "Butterfly"],
+	  	"artist":    ["Artist:", "Norman Connors"],
+	  	"label":     ["Label:", "Arista"],
+	  	"year":      ["Year:", "1977"],
+	  	"date":      ["Date:", "2013-22-4"],
+	  	"store":     ["Store:", "Other Music"],
+	  	"minutes":   ["Minutes:", "3"],
+	  	"comments":  ["Comments:", "Piano sample."],
+	  	"imported":  ["Import:", "Yes"],
+	  	"radio":     ["Release:", "Original"]
+
+     
+     
+     },
+     "sessionInfo6": {
+     	"optgroup":  ["Sampling:", "chorus"],
+     	"uname":     ["User:", "Spencer Garrett"],
+     	"email":     ["Email:", "Spencer@gmail.com"],
+     	"phone":     ["Phone:", "647-364-8887"],
+     	"album":     ["Album:", "Odyssey"],
+     	"song":      ["Song:", "Phire"],
+     	"artist":    ["Artist:", "Charles Earland"],
+     	"label":     ["Label:", "Mercury"],
+     	"year":      ["Year:", "1976"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+      },
+      "sessionInfo7":{
+     	"optgroup":  ["Sampling:", "verse"],
+     	"uname":     ["User:", "Rockey Dickey"],
+     	"email":     ["Email:", "Rockey@gmail.com"],
+     	"phone":     ["Phone:", "534-345-5432"],
+     	"album":     ["Album:", "I Love You"],
+     	"song":      ["Song:", "I Cried"],
+     	"artist":    ["Artist:", "Eddie Holman"],
+     	"label":     ["Label:", "ABC"],
+     	"year":      ["Year:", "1969"],
+     	"date":      ["Date:", "2012-25-1"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "4"],
+     	"comments":  ["Comments:", "Vocal sampled."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Original"]
+
+     		
+     		
+	  },
+	  "sessionInfo8":{
+	  	"optgroup":  ["Sampling:", "bridge"],
+	  	"uname":     ["User:", "Drew Michael"],
+	  	"email":     ["Email:", "drew@gmail.com"],
+	  	"phone":     ["Phone:", "989-908-9909"],
+	  	"album":     ["Album:", "Reaching For The World"],
+	  	"song":      ["Song:", "Hostage"],
+	  	"artist":    ["Artist:", "Harold Melvin & The Blue Notes"],
+	  	"label":     ["Label:", "ABC"],
+	  	"year":      ["Year:", "1976"],
+	  	"date":      ["Date:", "2011-22-4"],
+	  	"store":     ["Store:", "Blecker Bob's"],
+	  	"minutes":   ["Minutes:", "5"],
+	  	"comments":  ["Comments:", "string section sample."],
+	  	"imported":  ["Import:", "No"],
+	  	"radio":     ["Release:", "Original"]
+
+	  	
+
+     
+      },
+      "sessionInfo9":{
+       	"optgroup":  ["Sampling:", "bridge"],
+       	"uname":     ["User:", "Sarah Burkhardt"],
+       	"email":     ["Email:", "burkhardt@gmail.com"],
+       	"phone":     ["Phone:", "647-364-2212"],
+       	"album":     ["Album:", "Heaven & Earth"],
+       	"song":      ["Song:", "Run & Tell That"],
+       	"artist":    ["Artist:", "Heaven & Earth"],
+       	"label":     ["Label:", "Mercury"],
+       	"year":      ["Year:", "1978"],
+       	"date":      ["Date:", "2010-5-4"],
+       	"store":     ["Store:", "Bleecker Street"],
+       	"minutes":   ["Minutes:", "3"],
+       	"comments":  ["Comments:", "Used the drums."],
+       	"imported":  ["Import:", "No"],
+       	"radio":     ["Release:", "Original"]
+
+
+     
+     },
+     "sessionInfo10":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Jan Broberg"],
+     	"email":     ["Email:", "Jan@gmail.com"],
+     	"phone":     ["Phone:", "768-098-0987"],
+     	"album":     ["Album:", "Gene Chandler Situation"],
+     	"song":      ["Song:", "Simply Call It Love"],
+     	"artist":    ["Artist:", "Gene Chandler"],
+     	"label":     ["Label:", "Mercury"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Vibraphone sample."],
+     	"imported":  ["Import:", "Yes"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     
+     },
+     "sessionInfo11":{
+     	"optgroup":  ["Sampling:", "verse"],
+     	"uname":     ["User:", "Andrew Lauer"],
+     	"email":     ["Email:", "Lauer@gmail.com"],
+     	"phone":     ["Phone:", "647-878-9006"],
+     	"album":     ["Album:", "Bitches Brew"],
+     	"song":      ["Song:", "Spanish Key"],
+     	"artist":    ["Artist:", "Miles Davis"],
+     	"label":     ["Label:", "Columbia"],
+     	"year":      ["Year:", "1970"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+     },
+     "sessionInfo12":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Nate Bynum"],
+     	"email":     ["Email:", "Nate@gmail.com"],
+     	"phone":     ["Phone:", "631-364-0987"],
+     	"album":     ["Album:", "Goin' Places"],
+     	"song":      ["Song:", "I'll Be Understanding"],
+     	"artist":    ["Artist:", "Michael Henderson"],
+     	"label":     ["Label:", "Buddah"],
+     	"year":      ["Year:", "1977"],
+     	"date":      ["Date:", "2011-22-4"],
+     	"store":     ["Store:", "Other Music"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+  
+  
+     },
+     "sessionInfo13":{
+     	"optgroup":  ["Sampling:", "verse"],
+     	"uname":     ["User:", "Andrew Lander"],
+     	"email":     ["Email:", "andrew@gmail.com"],
+     	"phone":     ["Phone:", "647-364-7774"],
+     	"album":     ["Album:", "One"],
+     	"song":      ["Song:", "Nautilus"],
+     	"artist":    ["Artist:", "Bob James"],
+     	"label":     ["Label:", "CTI"],
+     	"year":      ["Year:", "1974"],
+     	"date":      ["Date:", "2011-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "7"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Original"]
+
+
+
+     },
+     "sessionInfo14":{
+     	"optgroup":  ["Sampling:", "chorus"],
+     	"uname":     ["User:", "Roy McCrerey"],
+     	"email":     ["Email:", "roy@gmail.com"],
+     	"phone":     ["Phone:", "647-364-8675"],
+     	"album":     ["Album:", "360 Degrees Of Billy Paul"],
+     	"song":      ["Song:", "I'm Just A Prisoner"],
+     	"artist":    ["Artist:", "Billy Paul"],
+     	"label":     ["Label:", "Philadelphia International"],
+     	"year":      ["Year:", "1972"],
+     	"date":      ["Date:", "2012-20-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "4"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+
+     },
+     "sessionInfo15":{
+     	"optgroup":  ["Sampling:", "bridge"],
+     	"uname":     ["User:", "Steve Caballero"],
+     	"email":     ["Email:", "steve@gmail.com"],
+     	"phone":     ["Phone:", "888-456-0985"],
+     	"album":     ["Album:", "Day In The Life"],
+     	"song":      ["Song:", "The Joker"],
+     	"artist":    ["Artist:", "Wes Montgomery"],
+     	"label":     ["Label:", "CTI/A&M"],
+     	"year":      ["Year:", "1967"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Other Music"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "For Drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Original"]
+
+
+
+     },
+     "sessionInfo16":{
+     	"optgroup":  ["Sampling:", "intro"],
+     	"uname":     ["User:", "Mike Vallely"],
+     	"email":     ["Email:", "Vallely@gmail.com"],
+     	"phone":     ["Phone:", "702-465-2920"],
+     	"album":     ["Album:", "Afro-Harping"],
+     	"song":      ["Song:", "Little Sunflower"],
+     	"artist":    ["Artist:", "Dorothy Ashby"],
+     	"label":     ["Label:", "Cadet/Verve"],
+     	"year":      ["Year:", "1968"],
+     	"date":      ["Date:", "2012-22-10"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the harp."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "verse"],
+     	"uname":     ["User:", "Cullen Moss"],
+     	"email":     ["Email:", "moss@gmail.com"],
+     	"phone":     ["Phone:", "878-352-1910"],
+     	"album":     ["Album:", "Soul Of The Bible"],
+     	"song":      ["Song:", "Space Spiritual"],
+     	"artist":    ["Artist:", "Cannonball Adderley"],
+     	"label":     ["Label:", "Capital"],
+     	"year":      ["Year:", "1972"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+      
+      
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "bridge"],
+     	"uname":     ["User:", "Lance Mountain"],
+     	"email":     ["Email:", "mountain@gmail.com"],
+     	"phone":     ["Phone:", "568-475-2938"],
+     	"album":     ["Album:", "Songs Of Experience"],
+     	"song":      ["Song:", "The Human Abstract"],
+     	"artist":    ["Artist:", "David Axelrod"],
+     	"label":     ["Label:", "Capital"],
+     	"year":      ["Year:", "1969"],
+     	"date":      ["Date:", "2011-22-4"],
+     	"store":     ["Store:", "Blecker Street"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "No"],
+     	"radio":     ["Release:", "Reissue"]
+
+     
+     
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "chorus"],
+     	"uname":     ["User:", "John Lucero"],
+     	"email":     ["Email:", "Lucero@gmail.com"],
+     	"phone":     ["Phone:", "647-364-0987"],
+     	"album":     ["Album:", "Release Of An Oath"],
+     	"song":      ["Song:", "Holy Are You"],
+     	"artist":    ["Artist:", "Electric Prunes"],
+     	"label":     ["Label:", "Reprise"],
+     	"year":      ["Year:", "1968"],
+     	"date":      ["Date:", "2012-22-4"],
+     	"store":     ["Store:", "Other Music"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "Yes"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+     },
+     "sessionInfo2": {
+     	"optgroup":  ["Sampling:", "verse"],
+     	"uname":     ["User:", "Ray Barbee"],
+     	"email":     ["Email:", "ray@gmail.com"],
+     	"phone":     ["Phone:", "647-465-2826"],
+     	"album":     ["Album:", "Histoire De Melody Nelson"],
+     	"song":      ["Song:", "Cargo Culte"],
+     	"artist":    ["Artist:", "Serge Gainsbourg"],
+     	"label":     ["Label:", "Phillips"],
+     	"year":      ["Year:", "1971"],
+     	"date":      ["Date:", "2010-16-4"],
+     	"store":     ["Store:", "Blecker Bob's"],
+     	"minutes":   ["Minutes:", "5"],
+     	"comments":  ["Comments:", "Used the drums."],
+     	"imported":  ["Import:", "Yes"],
+     	"radio":     ["Release:", "Reissue"]
+
+
+     	
+     }
+     
+}
